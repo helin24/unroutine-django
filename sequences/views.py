d203319f2de2cfe3d805820e64cbfd4118afe38b
Helin Shiah <helinx@fb.com>
2020-02-16 14:41:48 -0800

    Add form to change number of steps

--- sequences/views.py
+++ sequences/views.py
@@ -5,18 +5,22 @@ from .models import Edge, Transition
 
 # Create your views here.
 def index(request):
+    steps = 5
+    if request.POST:
+        steps = min(20, int(request.POST['steps']))
+
     # find all moves and select one at random
-    # find what edge it ends on
-    # find a move that starts on that one and continue
     current = Transition.objects.order_by("?").first()
     sequence = [current]
     count = 1
-    while count < 5:
+    while count < steps:
+        # find what edge it ends on
+        # find a move that starts on that one and continue
         current = Transition.objects.filter(entry=current.exit.id).order_by("?").first()
         sequence.append(current)
         count += 1
 
     template = loader.get_template('sequences/index.html')
-    context = {'transitions': sequence, 'startEdge': sequence[0].entry}
+    context = {'transitions': sequence, 'startEdge': sequence[0].entry, 'steps': steps}
     return HttpResponse(template.render(context, request))
 
