8a7bb063ebc58f552716f46863d5028eac51cb92
Helin Shiah <helin.shiah@gmail.com>
2020-02-15 20:02:20 -0800

    Show on admin

--- sequences/models.py
+++ sequences/models.py
@@ -6,11 +6,18 @@ class Move(models.Model):
     description = models.CharField(max_length=200)
     abbreviation = models.CharField(max_length=10)
 
+    def __str__(self):
+        return self.name
+
 class Edge(models.Model):
     name = models.CharField(max_length=50)
     abbreviation = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
 
 class Transition(models.Model):
     move = models.ForeignKey(Move, on_delete=models.CASCADE)
     entry = models.ForeignKey(Edge, on_delete=models.CASCADE, related_name='entry')
     exit = models.ForeignKey(Edge, on_delete=models.CASCADE, related_name='exit')
+
