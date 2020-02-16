87849e6c1fe3ad2a6dc38a346ac80a880d30df5e
Helin Shiah <helin.shiah@gmail.com>
2020-02-16 14:58:56 -0800

    Add optional direction to transitions

--- sequences/models.py
+++ sequences/models.py
@@ -17,9 +17,14 @@ class Edge(models.Model):
         return self.name
 
 class Transition(models.Model):
+    DIRECTION_CHOICES = [
+        ('CW', 'Clockwise'),
+        ('CCW', 'Counter-Clockwise'),
+    ]
     move = models.ForeignKey(Move, on_delete=models.CASCADE)
     entry = models.ForeignKey(Edge, on_delete=models.CASCADE, related_name='entry')
     exit = models.ForeignKey(Edge, on_delete=models.CASCADE, related_name='exit')
+    rotationDirection = models.CharField(max_length=3, choices=DIRECTION_CHOICES, null=True, blank=True)
 
     def __str__(self):
         return self.entry.abbreviation + ' -> ' + self.move.abbreviation + ' -> ' + self.exit.abbreviation
