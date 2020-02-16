976f9c05a6b0228e2036e7b45ca1bcb0964094de
Helin Shiah <helinx@fb.com>
2020-02-15 20:02:20 -0800

    Show on admin

--- sequences/admin.py
+++ sequences/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Edge, Move, Transition
+
+class EdgeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'abbreviation')
+
+class MoveAdmin(admin.ModelAdmin):
+    list_display = ('name', 'abbreviation')
+
+class TransitionAdmin(admin.ModelAdmin):
+    list_display = ('move', 'entry', 'exit')
+
+admin.site.register(Edge, EdgeAdmin)
+admin.site.register(Move, MoveAdmin)
+admin.site.register(Transition, TransitionAdmin)
