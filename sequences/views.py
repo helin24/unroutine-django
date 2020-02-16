525b4053a1ee6edeb3c5724274e901de3bdad669
Helin Shiah <helinx@fb.com>
2020-02-16 07:27:13 -0800

    Generate random sequences

--- sequences/views.py
+++ sequences/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Edge, Transition
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello! Welcome to unroutine sequences")
+    # find all moves and select one at random
+    # find what edge it ends on
+    # find a move that starts on that one and continue
+    current = Transition.objects.order_by("?").first()
+    sequence = [current]
+    count = 1
+    while count < 5:
+        current = Transition.objects.filter(entry=current.exit.id).order_by("?").first()
+        sequence.append(current)
+        count += 1
+
+    return HttpResponse(sequence[0].entry.abbreviation + " -> " +  " -> ".join(x.move.abbreviation for x in sequence) + " -> " + sequence[-1].exit.abbreviation)
