54c6dd3cb0d8322762afe56b994fc521534862d6
Helin Shiah <helinx@fb.com>
2020-02-15 19:44:18 -0800

    Add basic models

--- sequences/models.py
+++ sequences/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Move(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    abbreviation = models.CharField(max_length=10)
+
+class Edge(models.Model):
+    name = models.CharField(max_length=50)
+    abbreviation = models.CharField(max_length=10)
+
+class Transition(models.Model):
+    move = models.ForeignKey(Move, on_delete=models.CASCADE)
+    entry = models.ForeignKey(Edge, on_delete=models.CASCADE, related_name='entry')
+    exit = models.ForeignKey(Edge, on_delete=models.CASCADE, related_name='exit')
