34e851b741c92bf49f97bddf88692b80cd0876cf
Helin Shiah <helin.shiah@gmail.com>
2020-06-18 17:54:02 -0700

    Delete old random generator

--- sequences/views.py
+++ sequences/views.py
@@ -10,33 +10,6 @@ REPEATABLE = set(['TL', 'Loop', 'Bunny Hop'])
 MOVES_BEFORE_BACKSPIN = set(['FScSpin', 'FSitSpin', 'FCaSpin', 'FLbSpin', '3Turn'])
 BACKSPINS = set(['BScSpin', 'BSitSpin', 'BCaSpin'])
 
-def getContext(request, steps, cw):
-    excludeDirection = 'CCW' if cw else 'CW'
-
-    # find all moves and select one at random
-    availableTransitions = Transition.objects.select_related('move', 'entry', 'exit').exclude(rotationDirection=excludeDirection)
-    current = availableTransitions.order_by("?").first()
-    sequence = [current]
-    count = 1
-    while count < steps:
-        # find what edge it ends on
-        # find a move that starts on that one and continue
-        query = availableTransitions.filter(entry=current.exit.id)
-
-        # Exclude the same move unless it's repeatable
-        if current.move.abbreviation not in REPEATABLE:
-            query = query.exclude(id=current.id)
-
-        # Exclude backspins unless preceded by particular moves
-        if current.move.abbreviation not in MOVES_BEFORE_BACKSPIN:
-            query = query.exclude(move__abbreviation__in=BACKSPINS)
-
-        current = query.order_by("?").first()
-        sequence.append(current)
-        count += 1
-
-    return {'transitions': sequence, 'startEdge': sequence[0].entry, 'steps': steps, 'clockwise': cw}
-
 def index(request):
     steps = 5
     cw = False
@@ -58,7 +31,6 @@ def index(request):
 
     result['transitions'] = transitionsWithFoot
 
-    print(result)
     return HttpResponse(template.render(result, request))
 
 def json(request):
