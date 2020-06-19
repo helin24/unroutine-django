397369fc3cdcd4f534280e293f04c695d5f1b007
Helin Shiah <helin.shiah@gmail.com>
2020-06-19 16:24:22 -0700

    Add setup and simple test for generator

--- sequences/tests.py
+++ sequences/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from sequences.models import Move, Edge, Transition
+from sequences.generator import Generator
 
-# Create your tests here.
+class GeneratorTestCase(TestCase):
+    def setUp(self):
+        move = Move.objects.create(name="Three Turn", abbreviation="3Turn", changeFoot=False, initialLeftForC=None)
+        foEdge = Edge.objects.create(name="Forward Outside", abbreviation="FO")
+        biEdge = Edge.objects.create(name="Back Inside", abbreviation="BO")
+        self.transition = Transition.objects.create(move=move, entry=foEdge, exit=biEdge)
+
+    def testNextClockwiseIfInitialLeft(self):
+        start = Generator().nextClockwiseIfInitialLeft(onInitialFoot=True, clockwiseIfInitialLeft=None, transition=self.transition)
+        self.assertEquals(start, None)
+
