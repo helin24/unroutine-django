d6b98b9e4a8396b2ca06faa53da6fa5f72eb08e3
Helin Shiah <helin.shiah@gmail.com>
2020-07-03 11:08:03 -0700

    Refactor saving new moves from import

--- sequences/management/commands/import.py
+++ sequences/management/commands/import.py
@@ -13,12 +13,13 @@ class Command(BaseCommand):
     # TODO: Add only a single file
 
     def handle(self, *args, **options):
-        self.stdout.write('hello from handle')
-
         # look at all csv files
+        count = 0
         for entry in os.scandir(routineDirectory):
             print(entry.name)
             level, number, direction, sequenceType, letter = entry.name.split('.')[0].split('_')
+
+
 
             with open(entry.path, newline='') as csvfile:
                 reader = csv.reader(csvfile, delimiter=',')
@@ -29,48 +30,71 @@ class Command(BaseCommand):
                         move = maybeMove
                     else:
                         # Save new move
-                        print('Not Found: ' + moveName)
-                        description = input('Description?\n')
-                        abbreviation = input('Abbreviation?\n')
+                        move = self.saveNewMove(moveName, entryName, exitName)
 
-                        categoryLetter = input('Move, spin, or jump? (m, s, j)\n')
-
-                        try:
-                            category = next(filter(lambda choice: choice[0] == categoryLetter.capitalize(), CATEGORY_CHOICES))
-                        except:
-                            continue
-                        if category is None:
-                            continue
-                        else:
-                            category = category[0]
-                       
-                        ilfcLetter = input('Initial is left for clockwise? (t, f, or blank for neither)\n')
-                        if ilfcLetter == 't':
-                            initialLeftForC = True
-                        elif ilfcLetter == 'f':
-                            initialLeftForC = False
-                        elif ilfcLetter == '':
-                            initialLeftForC = None
-                        else:
-                            continue
-
-                        changeFoot = entryName[0] != exitName[0]
-
-                        move = Move(name=moveName, description=description, abbreviation=abbreviation, category=category, initialLeftForC=initialLeftForC, changeFoot=changeFoot)
-                        try:
-                            move.save()
-                            print('Saved: ' + move.name)
-                        except:
-                            print('Unable to save: ' + moveName)
+                    if move is None:
+                        print('Unable to import %s' % entry.name)
+                        continue
 
                     entryEdge = Edge.objects.filter(abbreviation=entryName[1:]).first()
                     exitEdge = Edge.objects.filter(abbreviation=exitName[1:]).first()
                     maybeTransition = Transition.objects.filter(entry=entryEdge.id).filter(exit=exitEdge.id).filter(move=move.id).first()
-                    if maybeTransition is None:
-                        # Save new edge
+                    if maybeTransition is not None:
+                        transition = maybeTransition
+                    else:
+                        # Save new transition
                         transition = Transition(move=move, entry=entryEdge, exit=exitEdge)
                         try:
                             transition.save()
                             print('Saved: %s -> %s -> %s' % (transition.entry.abbreviation, transition.move.name, transition.exit.abbreviation))
                         except:
                             print('Unable to save: %s -> %s -> %s' % (transition.entry.abbreviation, transition.move.name, transition.exit.abbreviation))
+
+                    if transition is None:
+                        print('Unable to import %s' % entry.name)
+                        continue
+
+
+
+            count += 1
+            if count > 5:
+                break
+
+    def saveNewMove(self, moveName, entryName, exitName):
+        print('Not Found: ' + moveName)
+        description = input('Description?\n')
+        abbreviation = input('Abbreviation?\n')
+
+        categoryLetter = input('Move, spin, or jump? (m, s, j)\n')
+
+        try:
+            category = next(filter(lambda choice: choice[0] == categoryLetter.capitalize(), CATEGORY_CHOICES))
+        except:
+            return None
+        if category is None:
+            return None
+        else:
+            category = category[0]
+       
+        ilfcLetter = input('Initial is left for clockwise? (t, f, or blank for neither)\n')
+        if ilfcLetter == 't':
+            initialLeftForC = True
+        elif ilfcLetter == 'f':
+            initialLeftForC = False
+        elif ilfcLetter == '':
+            initialLeftForC = None
+        else:
+            return None
+
+        changeFoot = entryName[0] != exitName[0]
+
+        move = Move(name=moveName, description=description, abbreviation=abbreviation, category=category, initialLeftForC=initialLeftForC, changeFoot=changeFoot)
+        try:
+            move.save()
+            print('Saved: ' + move.name)
+            return move
+        except:
+            print('Unable to save: ' + moveName)
+            
+        return None
+
