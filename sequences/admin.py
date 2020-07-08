ea8e686890a046361edde56b393dbc3694ca011f
Helin Shiah <helin.shiah@gmail.com>
2020-07-07 20:21:16 -0700

    Populate overall frequencies

--- sequences/admin.py
+++ sequences/admin.py
@@ -7,7 +7,7 @@ class EdgeAdmin(admin.ModelAdmin):
     list_display = ('name', 'abbreviation')
 
 class MoveAdmin(admin.ModelAdmin):
-    list_display = ('name', 'abbreviation', 'category', 'changeFoot', 'initialLeftForC')
+    list_display = ('name', 'abbreviation', 'category', 'changeFoot', 'initialLeftForC', 'frequency')
 
 class TransitionAdmin(admin.ModelAdmin):
     list_display = ('move', 'entry', 'exit')
